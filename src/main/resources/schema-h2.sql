30e80bdcfa6a613c5d37fa7a054d493debaee67b
SPatil40 <smita.patil40@t-mobile.com>
2020-12-28 13:43:54 +0530

    Revert "created project"
    This reverts commit 0e14e764

--- src/main/resources/schema-h2.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS EMPLOYEE;
-
-CREATE TABLE EMPLOYEE (
-        id NUMBER(10) NOT NULL,
-        firstName VARCHAR2(50) NOT NULL,
-        lastName VARCHAR2(50) NOT NULL,
-        PRIMARY KEY(id)
-        );
